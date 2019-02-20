2de276bd7619bf618ce59b1c37977e5fc8cb4335
amberm <nekosenpai@gmail.com>
2019-02-20 15:30:28 -0700

    I'm told these shoudl be more... descriptive

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require ('./softserver.js');
+
+server.listen(9000, ( ) => {
+    console.log("Heyo Captain Jack, Checking Stuff on the 9000 Rack!")
+})
---
+++ softserver.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userDB = require('./data/helpers/userDb');
+
+const softserver = express();
+
+softserver.use(express.json());
+softserver.use(helmet());
+
+
+// GONNA STUFF MY ENDPOINTS HERE...
+softserver.get('/', (req,res) => {
+    res.send(`<h2>Looks like you made it</h2>
+        <h2>Look How Far We've Come Together</h2>`);
+});
+
+// USERS ENDP's
+softserver.get('/users', async (reck, rez) => {
+    try{
+        const thing = await userDB.get(reck.query);
+        rez.status(200).json(thing);
+    }
+    catch (errerz) {
+        console.log(errerz);
+        rez.status(500).json({
+            disIssaMessage: "You ain\n't got nothin... ",
+        })
+    }
+});
+
+
+softserver.post('/users', async (rec, rez) => {
+    try{
+        const widget = await userDB.insert(rec.body);
+        rez.status(201).json(widget)
+    }
+    catch{
+        rez.status(500).json({ mess:'You Fool of a Took !!!'})
+    }
+});
+
+softserver.delete('/users/:id', async (rec, rex) => {
+    try {
+        var something = await userDB.remove(rec.params.id);
+        if (something) {
+            console.log(something);
+            rex.status(200).json({ randomtext: 'Balrog is dead,yo!'});
+        } 
+        else {
+            console.log(something);
+            rex.status(404).json({ morerandomtext: 'No Balrog here...'});
+        }
+        }
+        catch{
+            rez.status(500).json({ mess:'You Fool of a Took !!!'})
+        } 
+
+})
+
+softserver.put('/users/id', async (rec, rez) => {
+    try {
+        let killWho = await userDB.update(rec.params.id, rec.body);
+        if (killWho) {
+            rez.status(200).json({rando: "Updated the Elven Language"});
+        }
+        else {
+            rez.status(404).json({rando2: "No Ring Here. Only Golum"});
+        }
+    }
+    catch{
+        rez.status(500).json({ mess:'You Fool of a Took !!!'})
+    }
+})
+
+//GONNA STUFF MY FUNCTIONS HERE...
+
+
+
+module.exports = softserver;
